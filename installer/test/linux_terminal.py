233adb013c6f119693f2bec2b321659ef8997756
frefolli <f.refolli@campus.unimib.it>
2022-11-29 12:25:27 +0100

    boh

--- installer/test/linux_terminal.py
+++ installer/test/linux_terminal.py
@@ -40,3 +40,65 @@ class TestLinuxTerminal(unittest.TestCase):
         linux_terminal.create_directory(the_file)
         linux_terminal.delete_directory(the_file)
         the_dir.cleanup()
+
+    def test_fail_touch_file(self):
+        the_dir = tempfile.TemporaryDirectory()
+        linux_terminal = LinuxTerminal()
+        try:
+            linux_terminal.touch_file(f"{the_dir.name}/null/file")
+            self.assertTrue(False)
+        except Exception:
+            pass
+        the_dir.cleanup()
+
+    def test_fail_delete_file(self):
+        the_dir = tempfile.TemporaryDirectory()
+        linux_terminal = LinuxTerminal()
+        try:
+            linux_terminal.delete_file(f"{the_dir.name}/null/file")
+            self.assertTrue(False)
+        except Exception:
+            pass
+        the_dir.cleanup()
+
+    def test_fail_copy_file(self):
+        the_dir = tempfile.TemporaryDirectory()
+        linux_terminal = LinuxTerminal()
+        try:
+            linux_terminal.copy_file(f"{the_dir.name}/null/file {the_dir.name}/null2/file")
+            self.assertTrue(False)
+        except Exception:
+            pass
+        the_dir.cleanup()
+
+    def test_fail_move_file(self):
+        the_dir = tempfile.TemporaryDirectory()
+        linux_terminal = LinuxTerminal()
+        try:
+            linux_terminal.move_file(f"{the_dir.name}/null/file {the_dir.name}/null2/file")
+            self.assertTrue(False)
+        except Exception:
+            pass
+        the_dir.cleanup()
+
+
+    def test_fail_create_directory(self):
+        the_dir = tempfile.TemporaryDirectory()
+        linux_terminal = LinuxTerminal()
+        try:
+            linux_terminal.create_directory(f"{the_dir.name}/null/directory")
+            self.assertTrue(False)
+        except Exception:
+            pass
+        the_dir.cleanup()
+
+    def test_fail_delete_directory(self):
+        the_dir = tempfile.TemporaryDirectory()
+        linux_terminal = LinuxTerminal()
+        try:
+            linux_terminal.delete_directory(f"{the_dir.name}/null/directory")
+            self.assertTrue(False)
+        except Exception:
+            pass
+        the_dir.cleanup()
+
