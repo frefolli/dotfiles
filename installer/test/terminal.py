97e6c8566dd32c47dbf95a1a284bdd850389bd81
frefolli <f.refolli@campus.unimib.it>
2022-11-29 12:10:28 +0100

    split terminal tests

--- installer/test/terminal.py
+++ installer/test/terminal.py
@@ -4,54 +4,69 @@ import os
 from ..src.terminal import Terminal
 
 class TestTerminal(unittest.TestCase):
+    def test_touch_file(self):
+        the_dir = tempfile.TemporaryDirectory()
+        terminal = Terminal()
+        the_file = os.path.join(the_dir.name, "touch_file")
+        try:
+            terminal.touch_file(the_file)
+            self.assertTrue(False)
+        except Exception:
+            pass
+        the_dir.cleanup()
+
+    def test_delete_file(self):
+        the_dir = tempfile.TemporaryDirectory()
+        terminal = Terminal()
+        the_file = os.path.join(the_dir.name, "delete_file")
+        try:
+            terminal.delete_file(the_file)
+            self.assertTrue(False)
+        except Exception:
+            pass
+        the_dir.cleanup()
+
+    def test_copy_file(self):
+        the_dir = tempfile.TemporaryDirectory()
+        terminal = Terminal()
+        the_file = os.path.join(the_dir.name, "copy_file")
+        the_destination = os.path.join(the_dir.name, "copy_destination")
+        try:
+            terminal.copy_file(the_file, the_destination)
+            self.assertTrue(False)
+        except Exception:
+            pass
+        the_dir.cleanup()
+
     def test_move_file(self):
         the_dir = tempfile.TemporaryDirectory()
         terminal = Terminal()
-        the_file = os.path.join(the_dir.name, "test_move_file_1")
-        the_destination = os.path.join(the_dir.name, "test_move_file_2")
+        the_file = os.path.join(the_dir.name, "move_file")
+        the_destination = os.path.join(the_dir.name, "move_destination")
         try:
-            terminal.touch_file(the_file)
             terminal.move_file(the_file, the_destination)
-            terminal.delete_file(the_destination)
             self.assertTrue(False)
         except Exception:
             pass
         the_dir.cleanup()
     
-    def test_copy_file(self):
+    def test_create_directory(self):
         the_dir = tempfile.TemporaryDirectory()
         terminal = Terminal()
-        the_file = os.path.join(the_dir.name, "test_copy_file_1")
-        the_destination = os.path.join(the_dir.name, "test_copy_file_2")
+        the_directory = os.path.join(the_dir.name, "create_directory")
         try:
-            terminal.touch_file(the_file)
-            terminal.copy_file(the_file, the_destination)
-            terminal.delete_file(the_file)
-            terminal.delete_file(the_destination)
+            terminal.create_directory(the_directory)
             self.assertTrue(False)
         except Exception:
             pass
         the_dir.cleanup()
-        
-    def test_touch_delete_file(self):
+
+    def test_delete_file(self):
         the_dir = tempfile.TemporaryDirectory()
         terminal = Terminal()
-        the_file = os.path.join(the_dir.name, "test_touch_delete_file")
+        the_directory = os.path.join(the_dir.name, "delete_directory")
         try:
-            terminal.touch_file(the_file)
-            terminal.delete_file(the_file)
-            self.assertTrue(False)
-        except Exception:
-            pass
-        the_dir.cleanup()
-        
-    def test_create_delete_directory(self):
-        the_dir = tempfile.TemporaryDirectory()
-        terminal = Terminal()
-        the_file = os.path.join(the_dir.name, "test_create_delete_directory")
-        try:
-            terminal.create_directory(the_file)
-            terminal.delete_directory(the_file)
+            terminal.delete_directory(the_directory)
             self.assertTrue(False)
         except Exception:
             pass
